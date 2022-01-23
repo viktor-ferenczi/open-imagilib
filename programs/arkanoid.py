3e7328840d58fd49eda28490e9bddf91390b1981
Viktor Ferenczi <viktor@ferenczi.eu>
2022-01-23 23:03:15 +0100

    Arkanoid improved

--- programs/arkanoid.py
+++ programs/arkanoid.py
@@ -9,15 +9,14 @@ BALL_COLOR = W
 PLAYER_COLOR = (160, 160, 160)
 BRICK_COLORS = [R, G, B, Y, O, M, P, A]
 
-
 background(BACKGROUND_COLOR)
+
 total = 0
 for y in range(2, 4):
     for x in range(0, 8, 2):
         c = BRICK_COLORS[total % len(BRICK_COLORS)]
-        m[y][x] = m[y][x+1] = c
+        m[y][x] = m[y][x + 1] = c
         total += 1
-
 
 px = 4
 bx, by = px, 6
@@ -46,30 +45,35 @@ for _ in range(100):
     if by > 7:
         break
 
-    if m[by][bx] == PLAYER_COLOR:
+    if m[by][bx] == PLAYER_COLOR or m[by][bx - vx] == PLAYER_COLOR:
         by = 5
         vy = -1
 
     if m[by][bx] in BRICK_COLORS:
-        vy = -vy
         x = bx & 6
         m[by][x] = m[by][x + 1] = BACKGROUND_COLOR
         removed += 1
-        if removed == total:
-            break
+        vy = -vy
+        if 0 <= bx + vx < 8 and by + vy >= 0 and m[bx + vx][by + vy] in BRICK_COLORS:
+            vx = -vx
 
     m[by][bx] = BALL_COLOR
 
-    if vy > 0 and by > 2:
-        if px < 6 and bx > px + 1:
+    if by > 2:
+        if px < 6 and bx > px:
             px += 1
-        elif px > 1 and bx < px - 1:
+        elif px > 1 and bx < px:
             px -= 1
 
     for x in range(8):
         m[7][x] = PLAYER_COLOR if px - 1 <= x <= px + 1 else BACKGROUND_COLOR
 
-    a.add_frame(m, 50)
+    finished = removed == total
+
+    a.add_frame(m, 500 if finished else 50)
+
+    if finished:
+        break
 
 # ---
 
